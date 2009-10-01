86020d8f7a0f42126eeb48e9baa39907c49941ea
Richard Hart <richard@ur-ban.com>
2009-10-01 22:33:40 +0100

    Initial commit to auto_auth_spec.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'auto_auth_spec'
+
+class Test::Unit::TestCase
+end
